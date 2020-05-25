0e28f5d87f81b8ffaa523dea1941c6369a6a4b18
Eishita Yadav <ey4172@gmail.com>
2020-05-25 23:39:29 +0530

    Update traditional_time_series.py

--- traditional_time_series.py
+++ traditional_time_series.py
@@ -24,3 +24,55 @@ from statsmodels.tsa.arima_model import ARMA
 from statsmodels.tsa.arima_model import ARIMA
 from pmdarima.arima.utils import ndiffs
 
+# Load the dataset
+path ='../input/competitive-data-science-predict-future-sales/'
+sales_train = pd.read_csv(path+'sales_train.csv')
+
+# Creating the univariate time series 
+time_series  = pd.DataFrame(sales_train.groupby(['date_block_num'])['item_cnt_day'].sum())
+time_series.index= pd.date_range(start = '2013-01-01',end='2015-10-01', freq = 'MS')
+time_series = time_series.reset_index()
+time_series.columns = ['cummulative_month','item_cnt_month']
+
+# Visually diagnosing stationarity
+# A time series is stationary when: 
+# It isn't a function of time and doesn't have increasing / decreasing trends over time.
+# It is homoskedastic and the variance of the time series isn't a function of time.
+# The covariance of the ith and (i+m)th term should not be a function of time
+
+# Plotting the total sales across the months 
+sns.lineplot(x='cummulative_month',y='item_cnt_month',data = time_series)
+
+# Decompose the trends, seasonalities and residuals given in the time series
+ts = time_series['item_cnt_month']
+decomposition = sm.tsa.seasonal_decompose(ts, freq=12)  
+fig = plt.figure()  
+fig = decomposition.plot()  
+fig.set_size_inches(15, 8)
+
+# Plot the moving average and standard deviation of time series
+roll_mean = ts.rolling(window = 12, center = False).mean()
+roll_std = ts.rolling(window = 12, center = False).std()
+plt.plot(ts, color = 'blue',label = 'Original Data')
+plt.plot(roll_mean, color = 'red', label = 'Rolling Mean')
+plt.plot(roll_std, color ='black', label = 'Rolling Std')
+plt.xlabel('Time in Months')
+plt.ylabel('Total Sales')
+plt.legend(loc='best')
+plt.title('Rolling Mean & Standard Deviation')
+plt.show()
+
+# The rolling mean of the series shows a decreasing trend and the standard deviation also peaks around the times where the sales peak
+
+# Conduct the Augumented Dickey Fuller Test 
+# The Null hypothesis of this test assumes that the given time series isn't stationary 
+
+
+
+
+
+
+
+
+
+
